a47d33cf708e21da4aeac748f350de2daf0507e8
anass2666 <anassss672@gmail.com>
2026-05-26 12:02:03 +0200

    Create Parametres_Ref_Out.cs

---
+++ Parametres_Ref_Out.cs
@@ -0,0 +1,7 @@
+using System;
+
+class ParametresExemple
+{
+    // 1. Passage par ENTRÉE/SORTIE (ref)
+    // Les modifications réalisées dans le sous-programme s'appliquent sur la variable d'origine.
+    static void PlusUn(ref int a)
